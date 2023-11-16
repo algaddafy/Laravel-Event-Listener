79b974e0bf5d42594a96037771f1d07b84cb179d
nirobhasan-git <papon.nirob@gmail.com>
2023-11-16 15:35:15 +0600

    Complete setup Event&Listener with Controller

--- app/Http/Controllers/SubscribedController.php
+++ app/Http/Controllers/SubscribedController.php
@@ -2,11 +2,21 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\User;
+use App\Events\SendMail;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Event;
 
 class SubscribedController extends Controller
 {
     public function subscribed(){
-        dd("User subscribed");
+        $user = User::first('id');
+        
+        // dd($user->id);
+        Event::dispatch(new SendMail($user->id));
+        // Event::dispatch(new SendMail(2));
+        
+        // event(new SendMail(2));
+        // dd("User subscribed");
     }
 }
